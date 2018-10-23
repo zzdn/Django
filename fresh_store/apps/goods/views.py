cfd96ab701a5318fcc650c8290cacb70e92ac6b5
zzdn <13381174963@163.com>
2018-10-23 09:17:42 +0800

    登录页面验证修改

--- fresh_store/apps/goods/views.py
+++ fresh_store/apps/goods/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from ..user.models import *
+import re
 # Create your views here.
 
 def login(request):
     return render(request,'goods/login.html')
 
 def register(request):
-    return render(request,'goods/register.html')
+    if request.method == 'GET':
+        return render(request,'goods/register.html')
+    else:
+        username = request.POST.get("user_name")
+        password = request.POST.get("pwd")
+        email = request.POST.get("email")
+        allow = request.POST.get("allow")
+        if not all([username,password,email]):
+            return render(request, 'goods/register.html', {"merror": "请把数据填写完整"})
+        if allow !='on':
+            return render(request, 'goods/register.html', {"merror": "请勾选同意条款"})
+        try:
+            user =User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            return render(request, 'goods/register.html', {'merror': '用户名已存在'})
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+        return redirect(reverse('goods:index'))
+
 
 def index(request):
     return render(request,'goods/index.html')
